64463c295405340d313eea26b553120ee1e7bc16
Matt Paul <mattpaul001@icloud.com>
2019-04-10 11:17:28 +0100

    Create a few handlers

--- README.md
+++ README.md
@@ -8,3 +8,30 @@
 
 - [] As a user, i need to add a 'Trigger' to the app
 - [] As a user, i need to a add a 'Response' to the app, related to one or more triggers.
+
+**_How to run this project_**
+
+Install Minikube
+
+```
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-amd64 && \
+chmod +x minikube && \
+sudo mv minikube /usr/local/bin/
+```
+
+Install the driver
+
+```
+brew install docker-machine-driver-xhyve
+sudo chown root:wheel $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+sudo chmod u+s $(brew --prefix)/opt/docker-machine-driver-xhyve/bin/docker-machine-driver-xhyve
+```
+
+Start the minikube cluster - xhyve specifies that you are using 'Docker for mac' hypervisor
+`minikube start --vm-driver=xhyve`
+
+Set kubernetes cli to use our Minikube cluster
+`kubectl config use-context minikube`
+
+Verify that kubectl can communicate with the cluster
+`kubectl cluster-info`
---
+++ server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	// If an incoming request URL matches one of the paths, the corresponding handler is
+	// called, passing (http.ResponseWriter, *http.Request) as parameters
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/remedies", RemediesHandler)
+	r.HandleFunc("/triggers", TriggersHandler)
+	r.HandleFunc("/remedies/{category}/", RemediesCategoryHandler)
+	// Paths can have variables, defined using the format {name} or {name:pattern}
+	// r.HandleFunc("/triggers/{id:[0-9]+}", TriggerHandler)
+	http.Handle("/", r)
+
+	http.ListenAndServe(":80", r)
+}
+
+// HomeHandler handlers the "/" endpoint
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	x := "world"
+	fmt.Fprintf(w, "Fantastic stuff hello %v\n", x)
+}
+
+// RemediesHandler handles the "/remedies" endpoint
+func RemediesHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	x := "These are you're remedies"
+	fmt.Fprintf(w, "Right: %v\n", x)
+}
+
+// TriggersHandler handlers the "/triggers" endpoint
+func TriggersHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	x := "These are you're triggers"
+	fmt.Fprintf(w, "Remedies: %v\n", x)
+}
+
+// RemediesCategoryHandler handles "/remedies{category}"
+func RemediesCategoryHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Category: %v\n", vars["category"])
+}
